bf84720d415532cb65f74a2631a27f141528ef42
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-10-05 17:20:13 -0600

    add corellation matrix

--- app/main.py
+++ app/main.py
@@ -1,48 +1,32 @@
-import json
 import plotly.express as px
 import pandas as pd
-import pyarrow.parquet as pq
-import pyarrow as pa
-import time
-databaseFilePath="./training_data/Elysia.Laps.json"
-packetTrainingDataPath="./training_data/Elysia.Packets.json"
-def cleanData(df: pd.DataFrame):
-    df = df.drop(columns=["msgType", "_id.$oid"])
-    # drop rows where averagepackCurrent == NaN
+packetTrainingDataPath="./training_data/Elysia.Packets.feather"
+lapTrainingDataPath="./training_data/Elysia.Laps.feather"
+def cleanLapData(df: pd.DataFrame):
+    # TODO: Add more cleaning steps when packet structure is finalized.
+    df = df.drop(columns=["msgType", "_id.$oid", "averagepackCurrent.$numberDouble", "timestamp.$numberLong"])
     df = df.dropna(subset=["averagepackCurrent"])
     return df
 
-# with open(databaseFilePath, "r") as f:
-#     data = json.load(f)
-#     df = pd.json_normalize(data)
-#     cleanedDf = cleanData(df)
-#     print(cleanedDf)
-    # fig = px.line(cleanedDf, x="timestamp.$numberLong", y="amphours", title='Battery Usage Over Time')
-    # fig.show()
-
-
-# with open(packetTrainingDataPath, "r") as f:
-#     start_time = time.time()
-#     data = json.load(f)
-#     df = pd.json_normalize(data)
-#     print("JSON read time:", time.time() - start_time)
-# Reading JSON
-# start_time = time.time()
-# jsonDF = pd.read_json(packetTrainingDataPath)
-# print("JSON read time:", time.time() - start_time)
-
-# Reading CSV
-# start_time = time.time()
-# df_csv = pd.read_csv('output.csv')
-# print("CSV read time:", time.time() - start_time)
-# table = pa.Table.from_pandas(df_csv)
-# pq.write_table(table, 'output.parquet')
-start_time = time.time()
-df_read = pq.read_table('output.parquet').to_pandas()
-print("Parquet read time:", time.time() - start_time)
-start_time = time.time()
-df_feather = pd.read_feather('output.feather')
-print("Feather write time:", time.time() - start_time)
-# df_read.to_feather('output.feather')
-# df_loaded = pd.read_feather('output.feather')
-# print(df_loaded.head())
+def cleanPacketData(df: pd.DataFrame):
+    # TODO: Add more cleaning steps when packet structure is finalized.
+    df = df.drop(columns=["Battery.FanSpeed", "Battery.FanVoltage", "Battery.BMSRelayStatusFlags.MalfunctionIndicatorActive", "Battery.RequestedFanSpeed"])
+    return df
+def generateCorrelationMatrix(df: pd.DataFrame):
+    correlation_matrix = df.corr( numeric_only=True)
+    print(correlation_matrix)
+    fig = px.imshow(correlation_matrix)
+    fig.show()
+    return correlation_matrix
+def analyzePacketData(df: pd.DataFrame):
+    cleanedDf = cleanPacketData(df)
+    print(cleanedDf.head())
+    generateCorrelationMatrix(cleanedDf)
+def analyzeLapData(df: pd.DataFrame):
+    cleanedDF = cleanLapData(df)
+    print(cleanedDF.head())
+    generateCorrelationMatrix(cleanedDF)
+lapDataDF = pd.read_feather(lapTrainingDataPath)
+analyzeLapData(lapDataDF)
+packetDataDF = pd.read_feather(packetTrainingDataPath)
+analyzePacketData(packetDataDF)
