3fa632453ec946b40212460d65288306da488f94
Matthew <106455288+promatty@users.noreply.github.com>
2024-10-29 19:21:07 -0600

    packet data cleaned so that null and constant columns are dropped (#3)
    Co-authored-by: Justin Pham <113923596+justin-phxm@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 packetTrainingDataPath = "../training_data/Elysia.Packets.feather"
 lapTrainingDataPath = "../training_data/Elysia.Laps.feather"
-
 
 def cleanLapData(df: pd.DataFrame):
     # TODO: Add more cleaning steps when packet structure is finalized.
@@ -21,36 +21,39 @@ def cleanLapData(df: pd.DataFrame):
 
 def cleanPacketData(df: pd.DataFrame):
     # TODO: Add more cleaning steps when packet structure is finalized.
-    df = df.drop(
-        columns=[
-            "Battery.FanSpeed",
-            "Battery.FanVoltage",
-            "Battery.BMSRelayStatusFlags.MalfunctionIndicatorActive",
-            "Battery.RequestedFanSpeed",
-        ]
-    )
-    return df
+    #drop columns with null or empty values
+    cols_with_missing = [col for col in df.columns if df[col].isnull().all()]
+    df = df.drop(cols_with_missing, axis=1)
 
+    #drop columns which are all constant; they have no variance so correlation will be undefined/NaN
+    constant_columns = []
+    for col in df.columns:
+        #nunique doesn't work on lists or numpy arrays, which KeyMotor, MPPT, MotorDetails, and MotorFaults are. 
+        if isinstance(df[col][0], (list, np.ndarray)):
+            continue
+        #check if the column has only one unique value
+        if(df[col].nunique() == 1):
+            constant_columns.append(col)
+
+    df= df.drop(columns=constant_columns, axis=1)
+    return df
 
 def generateCorrelationMatrix(df: pd.DataFrame):
     correlation_matrix = df.corr(numeric_only=True)
     print(correlation_matrix)
     fig = px.imshow(correlation_matrix, title="Correlation Matrix for Helios Data")
-    # fig.show()
+    fig.show()
     return fig
-
 
 def analyzePacketData(df: pd.DataFrame):
     cleanedDf = cleanPacketData(df)
     print(cleanedDf.head())
     return generateCorrelationMatrix(cleanedDf)
 
-
 def analyzeLapData(df: pd.DataFrame):
     cleanedDF = cleanLapData(df)
     print(cleanedDF.head())
     return generateCorrelationMatrix(cleanedDF)
-
 
 def main():
     # lapDataDF = pd.read_feather(lapTrainingDataPath)
@@ -58,6 +61,6 @@ def main():
     packetDataDF = pd.read_feather(packetTrainingDataPath)
     return analyzePacketData(packetDataDF)
 
-
 if __name__ == "__main__":
     main()
+
