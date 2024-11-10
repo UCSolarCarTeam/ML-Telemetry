8b741604dff7573ddf63dd362c1ca476707eb57a
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-11-10 13:39:26 -0700

    updated file generation

--- app/scripts/generateFeather.py
+++ app/scripts/generateFeather.py
@@ -5,12 +5,6 @@ from pathnames import LAP_JSON_PATH, PACKET_JSON_PATH, TRAINING_DATA_FOLDER
 
 PACKET_FEATHER_PATH=f"{TRAINING_DATA_FOLDER}/Elysia.Packets.feather"
 LAP_FEATHER_PATH=f"{TRAINING_DATA_FOLDER}/Elysia.Laps.feather"
-def packetJSONToFeather(packetTrainingDF: pd.DataFrame):
-  packetTrainingDF.to_feather(PACKET_FEATHER_PATH)
-
-def lapJSONToFeather(lapTrainingDF: pd.DataFrame):
-  lapTrainingDF.to_feather(LAP_FEATHER_PATH)
-
 
 def main():
   if os.path.exists(PACKET_FEATHER_PATH) :
@@ -19,7 +13,7 @@ def main():
     with open(LAP_JSON_PATH) as json_file:
       data = json.load(json_file)
       lapTrainingDF = pd.json_normalize(data)   
-      lapJSONToFeather(lapTrainingDF)
+      lapTrainingDF.to_feather(LAP_FEATHER_PATH)
 
   if os.path.exists(LAP_FEATHER_PATH):
     print(f"{LAP_FEATHER_PATH} already exists. Delete them if you want to regenerate them.")
@@ -27,7 +21,7 @@ def main():
     with open(PACKET_JSON_PATH) as json_file:
       data = json.load(json_file)
       packetTrainingDF = pd.json_normalize(data)   
-      packetJSONToFeather(packetTrainingDF)
+      packetTrainingDF.to_feather(PACKET_FEATHER_PATH)
 
 
 if __name__ == "__main__":
