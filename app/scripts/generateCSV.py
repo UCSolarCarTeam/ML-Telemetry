c8338d22696c0f5046fd976cac3fd4d78e794bf8
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-11-10 13:43:47 -0700

    updated file generation (#7)

--- app/scripts/generateCSV.py
+++ app/scripts/generateCSV.py
@@ -8,41 +8,25 @@ from pathnames import LAP_JSON_PATH, PACKET_JSON_PATH, TRAINING_DATA_FOLDER, LAP
 PACKET_CSV_PATH=f'{TRAINING_DATA_FOLDER}/Elysia.Packets.csv'
 LAP_CSV_PATH=f'{TRAINING_DATA_FOLDER}/Elysia.Laps.csv'
 
-def generatePacketCSV():
-  if os.path.exists(PACKET_CSV_PATH):
-    print(f"{PACKET_CSV_PATH} already exists. Delete them if you want to regenerate them.")
+def generateCSV(outputFilePath: str, inputFeatherPath = "", inputJSONPath = "", ):
+  if os.path.exists(outputFilePath):
+    print(f"{outputFilePath} already exists. Delete them if you want to regenerate them.")
   else:
     data = None
-    if(os.path.exists(PACKET_FEATHER_PATH)):
-        data = pd.read_feather(PACKET_FEATHER_PATH)
-    elif os.path.exists(PACKET_JSON_PATH):
-      with open(PACKET_JSON_PATH) as json_file:
+    if(os.path.exists(inputFeatherPath)):
+        data = pd.read_feather(inputFeatherPath)
+    elif os.path.exists(inputJSONPath):
+      with open(inputJSONPath) as json_file:
         jsonData = json.load(json_file)
         data = pd.json_normalize(jsonData)   
     if(data is not None):
-      data.to_csv(PACKET_CSV_PATH)
+      data.to_csv(outputFilePath)
+      print(f"Generated {outputFilePath}")
     else:
-      print(f"Neither {PACKET_JSON_PATH} nor {PACKET_FEATHER_PATH} exists. Please generate them first or check that you are running this in the root.")
-
-def generateLapCSV():
-  if(os.path.exists(LAP_CSV_PATH)):
-    print(f"{LAP_CSV_PATH} already exists. Delete them if you want to regenerate them.")
-  else:
-    data = None
-    if(os.path.exists(LAP_FEATHER_PATH)):
-        data = pd.read_feather(LAP_FEATHER_PATH)
-    elif(os.path.exists(LAP_JSON_PATH)):
-      with open(LAP_JSON_PATH) as json_file:
-        jsonData = json.load(json_file)
-        data = pd.json_normalize(jsonData)   
-    if(data is not None):
-      data.to_csv(LAP_CSV_PATH)
-    else:
-      print(f"Neither {LAP_JSON_PATH} nor {LAP_FEATHER_PATH} exists. Please generate them first or check that you are running this in the root.")
+      print(f"Neither {inputFeatherPath} nor {inputFeatherPath} exists. Please generate them first or check that you are running this in the root.")
 
 def main():
-  generateLapCSV()
-  generatePacketCSV()
-
+  generateCSV(outputFilePath=LAP_CSV_PATH, inputJSONPath=LAP_JSON_PATH, inputFeatherPath=LAP_FEATHER_PATH)
+  generateCSV(outputFilePath=PACKET_CSV_PATH, inputJSONPath=PACKET_JSON_PATH, inputFeatherPath=PACKET_FEATHER_PATH)
 if __name__ == "__main__":
   main()
